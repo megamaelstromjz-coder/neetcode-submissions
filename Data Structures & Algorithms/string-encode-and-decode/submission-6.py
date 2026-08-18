b788612440e818015578dfb2e8ddf6093551bffa
megamaelstromjz-coder <megamaelstrom.jz@gmail.com>
2026-08-19 01:00:56 +0800

    Add: string-encode-and-decode - submission-6

---
+++ Data Structures & Algorithms/string-encode-and-decode/submission-6.py
@@ -0,0 +1,35 @@
+class Solution:
+
+    def encode(self, strs: List[str]) -> str:
+
+        output = ""
+
+        for s in strs:
+
+            if s == "":
+                output += "§"
+            else:
+                output += s + "±"
+        
+        return output 
+
+
+    def decode(self, s: str) -> List[str]:
+
+        op = []
+        lastSplice1=0
+        lastSplice2=0
+
+        for i in range(len(s)):
+            if s[i] == "±":
+                op.append(s[lastSplice1:i])
+                if i+1 < len(s):
+                    lastSplice1 = i + 1
+                else:
+                    break
+            if s[i] == "§":
+                op.append("")
+                if i+1 < len(s):
+                    lastSplice1 += 1
+
+        return op
