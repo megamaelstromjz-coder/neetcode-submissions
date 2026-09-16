08d47d1a5aa6ca95dec769e8df62b3d8310bded8
megamaelstromjz-coder <megamaelstrom.jz@gmail.com>
2026-09-17 07:01:52 +0800

    Add: power-of-two - submission-12

---
+++ Data Structures & Algorithms/power-of-two/submission-12.py
@@ -0,0 +1,15 @@
+class Solution:
+    def isPowerOfTwo(self, n: int) -> bool:
+
+        if n <= 0:
+            return False
+
+        if n == 1:
+            return True
+        
+        x = math.log2(n)
+
+        
+
+        return  2 ** int(x)  == n
+        
